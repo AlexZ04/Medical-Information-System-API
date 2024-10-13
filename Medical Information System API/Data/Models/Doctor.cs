dc3d350da4f933dd4608d99b564ff652a6b7712f
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-10-13 23:19:58 +0700

    add doctor models

---
+++ Medical Information System API/Data/Models/Doctor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Medical_Information_System_API.Data.Models
+{
+    public class Doctor
+    {
+
+    }
+
+    public class DoctorEditModel
+    {
+        [StringLength(Int32.MaxValue, MinimumLength = 1, ErrorMessage = "Email field must contain at least one character.")]
+        public string Email { get; set; }
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "Invalid length of name field.")]
+        public string Name { get; set; }
+        [AllowNull]
+        public DateTime Birthday { get; set; }
+        public Gender Gender { get; set; }
+        [AllowNull]
+        public string Phone { get; set; }
+    }
+
+    public class DoctorModel
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        [StringLength(Int32.MaxValue, MinimumLength = 1, ErrorMessage = "Name field must contain at least one character.")]
+        public string Name { get; set; }
+        [AllowNull]
+        public DateTime Birthday { get; set; }
+        public Gender Gender { get; set; }
+        [StringLength(Int32.MaxValue, MinimumLength = 1, ErrorMessage = "Email field must contain at least one character.")]
+        public string Email { get; set; } // ???
+        [AllowNull]
+        public string Phone { get; set; } // ???
+    }
+
+    public class DoctorRegistrationModel {
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "Invalid length of name field.")]
+        public string Name { get; set; }
+        [StringLength(Int32.MaxValue, MinimumLength = 6, ErrorMessage = "Password must contain at least 6 characters.")]
+        public string Password { get; set; }
+        [StringLength(Int32.MaxValue, MinimumLength = 1, ErrorMessage = "Email field must contain at least 1 character.")]
+        public string Email { get; set; }
+        [AllowNull]
+        public string Birthday { get; set; }
+        public Gender Gender { get; set; }
+        [AllowNull]
+        public string Phone { get; set; }
+        public string Speciality { get; set; } // ???
+    }
+}
---
+++ Medical Information System API/Data/Models/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace Medical_Information_System_API.Data.Models
+{
+    public enum Gender
+    {
+        Male = 0,
+        Female = 1
+    }
+}
